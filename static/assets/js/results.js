5b221ff1de8dda67363721e34690109519a24213
Louise Lalu <louiserafaellalu@gmail.com>
2023-11-30 23:05:43 +0800

    remove constants from results.js

--- static/assets/js/results.js
+++ static/assets/js/results.js
@@ -1,35 +1,3 @@
-const ROLES = [
-    "Senior Software Engineer",
-    "Marketing Coordinator",
-    "Data Scientist",
-    "Graphic Designer",
-    "Customer Support Specialist",
-]
-
-const COMPANIES = [
-    "Acme Innovations",
-    "Quantum Dynamics",
-    "Stellar Solutions",
-    "ByteCraft Technologies",
-    "Global Ventures Inc.",
-]
-
-const LOCATIONS = [
-    "San Francisco, CA, USA",
-    "Tokyo, Japan",
-    "Berlin, Germany",
-    "Sydney, Australia",
-    "Bangalore, India",
-]
-
-const DESCRIPTIONS = [
-    "Develop and maintain software applications, lead a team of developers, and contribute to the design and architecture of complex systems.",
-    "Plan and eecute marketing campaigns, manage social media accounts, and collaborate with cross-functional teams to drive brand awareness.",
-    "Analyze large datasets, build predictive models, and provide insights to support data-driven decision-making within the organization.",
-    "Create visually appealing designs for print and digital media, collaborate with clients to understand their needs, and stay updated on design trends.",
-    "Assist customers with product-related inquiries, troubleshoot issues, and provide excellent service to ensure customer satisfaction.",
-]
-
 posts = []
 
 function createJobPost(role, location, company, description) {
